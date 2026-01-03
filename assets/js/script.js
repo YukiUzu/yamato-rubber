fb4b7351d6628df2d0c051db30a3f5208cc570ad
YukiUzu <50851601+YukiUzu@users.noreply.github.com>
2026-01-04 00:50:28 +0900

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,32 @@
 // ローディング
 $(window).on('load', function() {
   var $loading = $("#loading");
-  const is_firstload = sessionStorage.getItem('is-first-load');
+  var is_firstload = sessionStorage.getItem('is-first-load');
   if (!is_firstload) {
     sessionStorage.setItem('is-first-load', true);
+    $loading.addClass("active");
+
     setTimeout(function(){
       $loading.fadeOut();
+      $("#mv").addClass("active");
     },3000);
   } else {
     $loading.hide();
+    $("#mv").addClass("active");
   }
 });
 
 $(function(){
-
+  $("#bg").slick({
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 60000,
+    cssEase: "linear",
+    slidesToShow: 1,
+    variableWidth: true,
+    swipe: false,
+    arrows: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+  })
 });
